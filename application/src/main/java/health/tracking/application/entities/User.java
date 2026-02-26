6aa48683433f909711c8e37458d311e457ab1fd6
rusuandreiraul <andreiraul.rusu@gmail.com>
2026-02-26 13:18:21 +0100

    backend: adaugare relatie manyToMany cu user pentru follow

--- application/src/main/java/health/tracking/application/entities/User.java
+++ application/src/main/java/health/tracking/application/entities/User.java
@@ -1,18 +1,22 @@
 package health.tracking.application.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@ToString
-@EqualsAndHashCode
+@ToString(exclude = {"following", "followers", "activities", "sleepList", "alimentationList", "goalList"})
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Table(name = "\"users\"")
 public class User {
     @Id
@@ -27,6 +31,18 @@ public class User {
     private double height;
     private LocalDate birthDate;
     private String urlProfileImage;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_follows",
+            joinColumns = @JoinColumn(name = "follower_id"),
+            inverseJoinColumns = @JoinColumn(name = "following_id")
+    )
+    private Set<User> following=new HashSet<>();
+
+    @ManyToMany(mappedBy = "following") //cei care ma urmaresc pe mine
+    @JsonIgnore
+    private Set<User> followers=new HashSet<>();
 
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Activity> activities;
