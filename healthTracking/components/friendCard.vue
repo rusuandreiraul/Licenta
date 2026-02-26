8263e9d0e6b6dd1ef963876c38aca5b673a6c0b8
rusuandreiraul <andreiraul.rusu@gmail.com>
2026-02-26 12:31:20 +0100

    frontend: creare card pentru prieteni si modificare pagina friend.vue cu afisare leaderbord si card pentru prieteni

---
+++ healthTracking/components/friendCard.vue
@@ -0,0 +1,84 @@
+<template>
+  <div
+    class="relative bg-neutral-primary-soft max-w-xs w-full p-6 border border-default rounded-base shadow-xs"
+  >
+    <button
+      id="dropdownButton"
+      data-dropdown-toggle="dropdown"
+      class="absolute top-2 end-2 text-body hover:text-heading bg-neutral-primary-soft box-border border border-transparent hover:bg-neutral-tertiary focus:ring-4 focus:ring-neutral-tertiary rounded-base p-1.5 focus:outline-none"
+      type="button"
+    >
+      <span class="sr-only">Open dropdown</span>
+      <svg
+        class="w-6 h-6"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <path
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-width="3"
+          d="M6 12h.01m6 0h.01m5.99 0h.01"
+        />
+      </svg>
+    </button>
+    <!-- Dropdown menu -->
+    <div
+      id="dropdown"
+      class="z-10 bg-neutral-primary-medium border border-default-medium rounded-base shadow-lg w-36 block hidden"
+    >
+      <ul
+        class="p-2 text-sm text-body font-medium"
+        aria-labelledby="dropdownButton"
+      >
+        <li>
+          <a
+            href="#"
+            class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded-md"
+            >Edit</a
+          >
+        </li>
+        <li>
+          <a
+            href="#"
+            class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded-md"
+            >Export Data</a
+          >
+        </li>
+        <li>
+          <a
+            href="#"
+            class="inline-flex items-center w-full p-2 text-fg-danger hover:bg-neutral-tertiary-medium rounded-md"
+            >Delete</a
+          >
+        </li>
+      </ul>
+    </div>
+    <div class="flex flex-col items-center">
+      <div class="relative">iconita cu strikeul cu persoana respectiva</div>
+      <img class="w-24 h-24 mb-6 rounded-full" alt="Bonnie image" />
+      <h5 class="mb-0.5 text-xl font-semibold tracking-tight text-heading">
+        Bonnie Green
+      </h5>
+      <span class="text-sm text-body">textul</span>
+      <div class="flex mt-4 md:mt-6 gap-4">
+        <button
+          type="button"
+          class="inline-flex items-center text-black bg-brand box-border border border- hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none"
+        >
+          Share
+        </button>
+        <button
+          type="button"
+          class="inline-flex self-start w-auto text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none"
+        >
+          Message
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
--- healthTracking/pages/friend.vue
+++ healthTracking/pages/friend.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from "vue";
+import friendCard from "~/components/friendCard.vue";
 const friendName = ref("");
 const friendList = ref([]);
 
@@ -24,23 +25,54 @@ async function getUsers() {
 <template>
   <Sidebar />
   <main class="flex-1 p-4 sm:ml-64 flex gap-6 bg-gray-100">
-    <div class="flex flex-col gap-2 bg-white w-full">
-      <div class="p-2 gap-2 flex flex-row justify-between items-start">
-        <div class="flex gap-4 p-3 items-end">
+    <div class="flex flex-col gap-2 p-2 bg-white w-full">
+      <div
+        class="flex flex-col lg:flex-row p-4 gap-6 bg-white w-full items-center lg:items-end justify-between border border-green-500 rounded-xl shadow-sm"
+      >
+        <div
+          class="flex gap-2 sm:gap-4 items-end justify-center p-2 w-full lg:w-auto"
+        >
           <div class="flex flex-col items-center">
-            <div class="bg-gray-300 h-24 w-20">2</div>
-            <span>Argint</span>
+            <div
+              class="bg-gray-300 w-16 sm:w-20 h-20 sm:h-24 flex items-center justify-center font-bold text-white rounded-t-lg"
+            >
+              2
+            </div>
+            <span class="text-sm font-medium mt-1">Argint</span>
           </div>
+
           <div class="flex flex-col items-center">
-            <div class="bg-yellow-400 h-32 w-20">1</div>
-            <span>aur</span>
+            <div
+              class="bg-yellow-400 w-16 sm:w-20 h-28 sm:h-32 flex items-center justify-center font-bold text-white rounded-t-lg shadow-md"
+            >
+              1
+            </div>
+            <span class="text-sm font-bold mt-1 uppercase text-yellow-600"
+              >Aur</span
+            >
           </div>
-          <div class="flex items-center flex-col">
-            <div class="bg-orange-400 h-20 w-20">3</div>
-            <span>bronz</span>
+
+          <div class="flex flex-col items-center">
+            <div
+              class="bg-orange-400 w-16 sm:w-20 h-16 sm:h-20 flex items-center justify-center font-bold text-white rounded-t-lg"
+            >
+              3
+            </div>
+            <span class="text-sm font-medium mt-1">Bronz</span>
           </div>
         </div>
-        <div class="flex flex-col gap-2">
+        <div
+          class="flex flex-col gap-4 p-4 bg-gray-50 border border-gray-200 rounded-2xl w-full lg:max-w-xs shadow-sm"
+        >
+          <div class="text-center font-semibold text-gray-700">
+            🏆 Eu vs PopescuMaria
+          </div>
+          <div class="flex justify-center gap-3">
+            <UButton variant="outline" class="flex-1">Share</UButton>
+            <UButton class="flex-1">Message</UButton>
+          </div>
+        </div>
+        <div class="flex flex-col gap-2 w-full lg:w-64 lg:order-3">
           <i>Cauta prieteni</i>
           <UInput
             icon="i-lucide-search"
@@ -63,8 +95,16 @@ async function getUsers() {
         </div>
       </div>
       <div>
-        lista de prieteni cu care pot sa dau un high five, sa veririfc streakul
-        si sa pot trimitr mesaje
+        <div class="mb-6">
+          <h1 class="font-bold text-2xl p-3 text-center">My Feed</h1>
+        </div>
+        <div
+          class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center mx-auto px-4"
+        >
+          <FriendCard />
+          <friendCard />
+          <friendCard />
+        </div>
       </div>
     </div>
   </main>
